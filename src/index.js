c26f8680204ca1d05f547634f47b7a57fde1d4bb
peyman ahmadian <ahmadian.peyman@gmail.com>
2020-11-22 03:17:32 +0330

    setup RTL component,setup main page

--- src/index.js
+++ src/index.js
@@ -2,12 +2,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {BrowserRouter} from "react-router-dom";
+import {ConfigProvider} from "antd";
 //import component
 import App from './view/App';
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-        <App />
+        <ConfigProvider direction="rtl">
+            <App />
+        </ConfigProvider>
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
--- src/view/layout/content.jsx
+++ src/view/layout/content.jsx
@@ -1,14 +1,19 @@
-import {Col, Layout, Row} from "antd";
+import {Layout} from "antd";
+import {Switch,Route} from "react-router-dom";
 import Error from "../error/error";
+//import component
+import Home from "../home";
+//
 const Context=(props)=>{
     return(
-        <Layout.Content>
-            <Row justify="center">
-                <Col xs={20} className={"content"}>
-                    <Error/>
-                </Col>
-            </Row>
+        <Layout.Content className={"content"}>
+            <Switch>
+                <Route path={"/"} exact={true} component={Home}/>
+                <Route component={Error}/>
+            </Switch>
         </Layout.Content>
     )
 }
 export default Context;
+//todo
+//create input for Error component for 404
